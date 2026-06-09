10befba32551b27e7da360ab04aaa365f910e1d3
aj07256768-sys <aj07256768@gmail.com>
2026-06-09 15:24:18 +0300

    updated

--- Src/models.py
+++ Src/models.py
@@ -3,4 +3,26 @@ class Transaction:
     def __init_(self,Amount:float,category:str,Description:str= ""):
         self.Amount = Amount
         self.category = category
-        self.Description = Description
+        self.Description = Description 
+
+    def To_dic(self) -> dic:
+        return {
+            "Amount":self.Amount,
+            "category":self.category,
+            "Description":self.Description
+
+        } 
+
+
+
+class Expense(Transaction):
+    def __init_(self,Amount,category,Description):
+        super().__init_(Amount,Description,category)  
+        self.type='Expense' 
+
+
+
+class Income(Transaction):
+    def __init_(self,Amount,Description,category):
+        super().__init_(Amount,category,Description)
+        self.type='Income'
